d8ff6995b76e4ae5a7dd60f6d6fdcdb412b3e420
DibinDinesh <dibindinesh4501@gmail.com>
2024-07-01 13:14:39 +0530

    message updatd

---
+++ store/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from store.models import Size,Brand,Category,Product,Tag
+
+admin.site.register(Size)
+
+admin.site.register(Brand)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Tag)
